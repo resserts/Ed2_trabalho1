bf29efafaa9854957a7d0346297c5c8911a264dc
Tiago <61522202+resserts@users.noreply.github.com>
2025-05-30 09:13:19 -0300

    comentario circulo.h

--- circulo.h
+++ circulo.h
@@ -9,6 +9,7 @@ Info criaCirculo(int i, double x, double y, double r, char* corb, char* corp);
 void moveCirc(Info i, double x, double y);
 // Muda x e y do circulo i passado para x e y.
 
+// Retornam os atributos do circulo i.
 int getCircId(Info i);
 double getCircx(Info i);
 double getCircy(Info i);

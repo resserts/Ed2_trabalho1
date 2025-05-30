7f680298d94fb878734fbf4dffc6f782da6a2c84
Tiago <61522202+resserts@users.noreply.github.com>
2025-05-30 08:58:19 -0300

    comentario query.h

--- query.h
+++ query.h
@@ -3,5 +3,6 @@
 #include "smutreap.h"
 
 void comandosQuery(SmuTreap t, char* fn, char* saida);
+// Executa os comandos do arquivo query fn sobre a SmuTreap t e salva os arquivos gerados em saida.
 
 #endif

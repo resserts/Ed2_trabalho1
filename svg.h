a15de0be5f5c5626489d4d72173c6e265f09de28
Tiago <61522202+resserts@users.noreply.github.com>
2025-05-30 09:06:19 -0300

    comentario svg.h

--- svg.h
+++ svg.h
@@ -11,6 +11,9 @@ enum tipo{RETANGULO, CIRCULO, LINHA, TEXTO};
 typedef void* Letra;
 
 Letra criaLetra(double x, double y, char l);
+// Retorna um ponteiro para uma Letra l com posição=(x,y).
+
 void gerarSvg(SmuTreap t, char* fn, Lista letras);
+// Gera o svg fn usando as formas na SmuTreap t e a lista letras, caso não seja nula.
 
 #endif
